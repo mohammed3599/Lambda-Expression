04c8322226698dfb2ce9f71c1fff90ae62b4336a
mohammed3599 <kataloni1999@gmail.com>
2023-07-24 12:50:43 +0400

    git by age Function

---
+++ Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_Expression
+{
+    internal class Person
+    {
+
+        public String Name { get; set; }
+        public int Age { get; set; }
+        public String Gender { get; set; }
+        public String Address { get; set; }
+
+        public Person(String Name, int Age, String Gender, String Address)
+        {
+            this.Name = Name;
+            this.Age = Age;
+            this.Gender = Gender;
+            this.Address = Address;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,26 +5,82 @@
         static void Main(string[] args)
         {
             // Func<int, int> square = x => x * x;
-            Func<int, int> square = (x) =>
+            //Func<int, int> square = (x) =>
+            //{
+            //    Console.WriteLine(x);
+            //    return x * x;
+            //};
+            //int result = square(6);
+            //    Console.WriteLine(result);
+
+            //MathOperation mathOperation = add;
+            //int result2 = mathOperation(3,5);
+            //Console.WriteLine(result2);
+
+            //mathOperation = multiply;
+            //int result3 = mathOperation(4,6);
+            //Console.WriteLine(result3);
+
+            List<Person> list = new List<Person>()
             {
-                Console.WriteLine(x);
-                return x * x;
+                new Person("Salim", 29, "Male", "Muscat"),
+                new Person("Fatema", 25, "Female", "Ibri"),
+                new Person("Ali", 32, "Male", "Sohar"),
+                new Person("Maha", 22, "Female", "Salalah"),
+                new Person("Saeed", 22, "Male", "Nizwa"),
+                new Person("Mohammed", 24, "Male", "Rustaq"),
+                new Person("Reem", 30, "Female", "Barka"),
+                new Person("Sheikha", 20, "Female", "Muscat"),
+                new Person("Abddulah", 34, "Male", "Salalah"),
             };
-            int result = square(6);
-                Console.WriteLine(result);
 
-            MathOperation mathOperation = add;
-            int result2 = mathOperation(3,5);
-            Console.WriteLine(result2);
+            //List<Person> persons = list
+            //    .Where(x => x.Age <= 25)
+            //    .OrderBy(x => x.Name)
+            //    .ToList();
+            //List<Person> persons = list.First(x => x.Address == "Muscat");
+            //Console.WriteLine(persons);
 
-            mathOperation = multiply;
-            int result3 = mathOperation(4,6);
-            Console.WriteLine(result3);
+
+            List<String> Names = list
+                                     .Where(x => x.Age <= 25)
+                                     .OrderBy(x => x.Name)
+                                     .Select(x => x.Name)
+                                     .ToList();
+
+            Person? person = list .FirstOrDefault(x => x.Address == "Muscat");
+            if (person == null)
+            {
+                Console.WriteLine(person.Name);
+            }
+            Console.WriteLine(person?.Name);
+            Console.WriteLine(person?.Name?? "Hello");
+        }
+
+        public List<Person> getByAge (int age, List<Person> persons)
+        {
+            List<Person> agePerson = new List<Person>();
+            foreach (Person person in persons)
+            {
+                if (person.Age > age)
+                {
+                    agePerson.Add(person);
+                }
+            }
+            return agePerson;
+        }
+        static void printPersonList(List<Person> persons)
+        {
+            foreach (Person person in persons)
+            {
+                Console.WriteLine(person.Name);
+            }
         }
 
         public delegate int MathOperation(int a, int b);
         static int add(int a, int b) => a+b;
         static int multiply(int a, int b) => a*b;
         static void abc() { }
+
     }
 }
